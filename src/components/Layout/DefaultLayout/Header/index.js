16952241946a88bf8249144f1368aaf9db66a60a
danhnguyen138 <38952742+danhnguyen138@users.noreply.github.com>
2023-02-09 16:08:07 +0700

    css update

--- src/components/Layout/DefaultLayout/Header/index.js
+++ src/components/Layout/DefaultLayout/Header/index.js
@@ -1,13 +1,35 @@
 
 import classNames from 'classnames/bind';
 import styles from './Header.module.scss'
+import images from '~/assets/images';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCircleXmark, faSpinner, faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
+ 
 const cx=classNames.bind(styles)
 function Header() {
 
     return (
         <header className={cx('wrapper')}>
            <div className={cx('inner')}>
-            
+                <div className={cx('logo')}>
+                    <img src={images.logo} alt="Tiktok"/>
+                </div>
+                <div className={cx('search')}>
+                    <input placeholder="Search accounts and videos" spellCheck={false}/>
+                    <button className={cx('clear')}>
+                        {/* Clear */}
+                        <FontAwesomeIcon icon={faCircleXmark} />
+
+                    </button>
+                    <FontAwesomeIcon className={cx('loading')} icon={faSpinner} />
+                    {/* Loading */}
+                    <button className={cx('search-button')}>
+                        <FontAwesomeIcon  icon={faMagnifyingGlass} />
+                    </button>
+                </div>
+                <div className={cx('action')}>
+                    <input placeholder='HEllo world'/>
+                </div>
            </div>
         </header>
     );
